bdda00ef0928854fef5276c9c3bb9c5c9c5238ce
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-05-12 12:25:46 +0530

    ''

--- package/src/layouts/full/header/Topbar.tsx
+++ package/src/layouts/full/header/Topbar.tsx
@@ -8,107 +8,95 @@ const Topbar = () => {
     const dropdownItems = [
         {
             id: 1,
-            title: "React Preview",
+            img: "src/assets/images/svgs/react-cat-icon.svg",
+            title: "React Version",
             href: "https://www.wrappixel.com/templates/materialm-tailwind-react-admin-template/?ref=376#demos"
         },
         {
             id: 2,
-            title: "Bootstrap Preview",
-            href: "https://www.wrappixel.com/templates/materialm-admin-dashboard-template/?ref=376#demos"
-        },
-        {
-            id: 3,
-            title: "Angular Preview",
+            img: "src/assets/images/svgs/angular-cat-icon.svg",
+            title: "Angular Version",
             href: "https://www.wrappixel.com/templates/materialm-material-angular-dashboard-template/?ref=376#demos"
         },
         {
-            id: 4,
-            title: "Vuejs Preview",
+            id: 3,
+            img: "src/assets/images/svgs/vue-cat-icon.svg",
+            title: "Vuejs Version",
             href: "https://www.wrappixel.com/templates/materialm-vuejs-vuetify-admin-template/?ref=376#demos"
         },
         {
+            id: 4,
+            img: "src/assets/images/svgs/nuxt-cat-icon.svg",
+            title: "Nuxtjs Version",
+            href: "https://www.wrappixel.com/templates/materialm-vuetify-nuxt-js-admin-template/?ref=376#demos"
+        },
+        {
             id: 5,
-            title: "Nextjs Preview",
+            img: "src/assets/images/svgs/react-cat-icon.svg",
+            title: "NextJs Version",
             href: "https://www.wrappixel.com/templates/materialm-next-js-tailwind-dashboard-template/?ref=376#demos"
         },
 
         {
             id: 6,
-            title: "Buy React Version",
-            href: "https://www.wrappixel.com/templates/materialm-tailwind-react-admin-template/?ref=376#package-card"
-        },
-        {
-            id: 7,
-            title: "Buy Bootstrap Version",
-            href: "https://www.wrappixel.com/templates/materialm-admin-dashboard-template/?ref=376#package-card"
-        },
-        {
-            id: 8,
-            title: "Buy Angular Version",
-            href: "https://www.wrappixel.com/templates/materialm-material-angular-dashboard-template/?ref=376#package-card"
-        },
-        {
-            id: 9,
-            title: "Buy Vuejs Version",
-            href: "https://www.wrappixel.com/templates/materialm-vuejs-vuetify-admin-template/?ref=376#package-card"
-        },
-        {
-            id: 10,
-            title: "Buy Nextjs Version",
-            href: "https://www.wrappixel.com/templates/materialm-next-js-tailwind-dashboard-template/?ref=376#package-card"
+            img: "src/assets/images/svgs/bt-cat-icon.svg",
+            title: "Bootstrap Version",
+            href: "https://www.wrappixel.com/templates/materialm-admin-dashboard-template/?ref=376#demos"
         },
 
     ]
     return (
-        <div className="py-3 px-4 bg-dark z-40 sticky top-0">
-            <div className="flex items-center lg:justify-between flex-wrap justify-center">
-                <div className="flex items-center gap-12">
-                    <img src={wrappixel_logo} alt="logo" />
-                    <div className="lg:flex items-center gap-3 hidden">
-                        <Link to="https://support.wrappixel.com/" className="flex items-center gap-1 text-white bg-transparent hover:bg-primary py-2 px-3 rounded-md">
-                            <Icon icon="tabler:lifebuoy" className="shrink-0 text-[19px]" />
-                            <h4 className="text-sm font-medium leading-none text-white">Support</h4>
+        <div className="py-[15px] px-6 z-40 sticky top-0 bg-[linear-gradient(90deg,_#0f0533_0%,_#1b0a5c_100%)]">
+            <div className="flex flex-col lg:flex-row gap-4 items-center justify-between">
+                <div className="md:flex hidden items-center gap-5">
+                    <Link target="_black" to="https://www.wrappixel.com">
+                        <img src={wrappixel_logo} alt="logo"  width={147}/> 
+                    </Link>
+                    <div className="xl:flex items-center gap-4 pl-5 border-l border-opacity-20 border-white hidden">
+                        <Link target="_black" to="https://www.wrappixel.com/templates/category/react-templates/" className="flex items-center gap-2 text-white bg-transparent hover:text-[#714bff]">
+                            <Icon icon="solar:window-frame-linear" width={20} />
+                            <h4 className="text-base font-normal leading-none text-white hover:text-[#714bff]">Templates</h4>
                         </Link>
-                        <Link to="https://www.wrappixel.com" className="flex items-center gap-1 text-white bg-transparent hover:bg-primary py-2 px-3 rounded-md">
-                            <Icon icon="tabler:gift" className="shrink-0 text-[19px]" />
-                            <h4 className="text-sm font-medium leading-none text-white">Templates</h4>
+                        <Link target="_black" to="https://support.wrappixel.com/" className="flex items-center gap-2 text-white bg-transparent hover:text-[#714bff]">
+                            <Icon icon="solar:question-circle-linear" width={20} />
+                            <h4 className="text-base font-normal leading-none text-white hover:text-[#714bff]">Help</h4>
                         </Link>
-                        <Link to="https://www.wrappixel.com/hire-us/" className="flex items-center gap-1 text-white bg-transparent hover:bg-primary py-2 px-3 rounded-md">
-                            <Icon icon="tabler:briefcase" className="shrink-0 text-[19px]" />
-                            <h4 className="text-sm font-medium leading-none text-white">Hire Us</h4>
+                        <Link target="_black" to="https://www.wrappixel.com/hire-us/" className="flex items-center gap-2 text-white bg-transparent hover:text-[#714bff]">
+                            <Icon icon="solar:case-round-linear" width={20} />
+                            <h4 className="text-base font-normal leading-none text-white hover:text-[#714bff]">Hire Us</h4>
                         </Link>
                     </div>
                 </div>
-                <div className="flex items-center gap-2 flex-wrap justify-center lg:mt-0 mt-2">
-                    <h4 className="text-lg font-medium text-white">Check MaterialM Premium Version</h4>
-                    <Dropdown label={
-                        <div className="flex items-center gap-1">
-                            <Icon icon="tabler:device-laptop" className="text-lg" />
-                            <p className="text-[15px]">Live Preview</p>
+                <div className="flex flex-col md:flex-row items-center gap-4 justify-center">
+                    <h4 className="text-sm font-semibold text-white uppercase bg-[linear-gradient(90deg,_#FFFFFF_0%,_#8D70F8_100%)] [-webkit-background-clip:text] [background-clip:text] [-webkit-text-fill-color:transparent]">Checkout Pro Version</h4>
+                    <div className="flex flex-col sm:flex-row items-center gap-[10px]">
+                        <div className="flex items-center gap-[10px]">
+                            <div className="live-preview-drop !rounded-sm border border-[#ffffff66] border-1 hover:bg-[#8d70f8]">
+                                <Dropdown label={
+                                    <p className="text-base text-white font-normal">Live Preview</p>
+                                } color="" size="sm" className="py-3 px-4 text-white" >
+                                    {
+                                        dropdownItems.map((item) => {
+                                            return (
+                                                <Dropdown.Item to={item.href} key={item.id} className="flex items-center gap-3 text-base text-[#000c29] py-3 px-[18px] group rounded-sm hover:bg-[#000c290d] hover:text-[#000c29]" as={Link} href={item.href} target='_blank' icon={() =>
+                                                    <img src={item.img} width={18} alt="logo" />}>
+                                                    <span className="group-hover:text-[#000c29]" >{item.title}</span>
+                                                </Dropdown.Item>
+                                            )
+                                        })
+                                    }
+                                </Dropdown>
+                            </div>
+                            <Link target="_black" to="https://www.wrappixel.com/templates/materialm-tailwind-react-admin-template/?ref=376" className="flex items-center px-4 py-[11px] rounded-sm gap-2 text-white bg-[#8d70f8] hover:bg-[#714bff]">
+                                <Icon icon="solar:crown-linear" width={18} />
+                                <h4 className="text-base font-normal leading-none text-white">Get Pro</h4>
+                            </Link>
                         </div>
-                    } color="outlineprimary" size="sm" className="py-2" >
-                        {
-                            dropdownItems.slice(0,5).map((item) => {
-                                return (
-                                    <Dropdown.Item className="flex items-center gap-1 text-sm py-2.5 px-4 group" target="_blank" as={Link} to={item.href} icon={() => <Icon icon="tabler:external-link" className="text-lg text-link group-hover:text-primary" />}>{item.title}</Dropdown.Item>
-                                )
-                            })
-                        }
-                    </Dropdown>
-                    <Dropdown label={
-                        <div className="flex items-center gap-1">
-                            <Icon icon="tabler:shopping-cart" className="text-lg" />
-                            <p className="text-[15px]">Buy Now</p>
-                        </div>
-                    } color="primary" size="sm" >
-                        {
-                            dropdownItems.slice(5,10).map((item) => {
-                                return (
-                                    <Dropdown.Item className="flex items-center gap-1 text-sm py-2.5 px-4 group" target="_blank" as={Link} to={item.href} icon={() => <Icon icon="tabler:external-link" className="text-lg text-link group-hover:text-primary" />}>{item.title}</Dropdown.Item>
-                                )
-                            })
-                        }
-                    </Dropdown>
+                        <Link target="_black" to="https://www.wrappixel.com/all-access-pass/?ref=376" className="flex items-center px-4 py-[11px] rounded-sm gap-2 text-black bg-[#b3f143] hover:bg-[#90de03]">
+                            <Icon icon="solar:bolt-linear" width={18} />
+                            <h4 className="text-base font-normal leading-none">All Access Pass</h4>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
